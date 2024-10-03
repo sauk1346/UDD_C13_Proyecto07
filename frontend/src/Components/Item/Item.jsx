bea639f148e7399e6c6c73099bc61e2fafbd99cd
sauk1346 <sauk1346@gmail.com>
2024-10-03 19:38:35 -0300

    frontend: componente Item

--- frontend/src/Components/Item/Item.jsx
+++ frontend/src/Components/Item/Item.jsx
@@ -1,6 +1,13 @@
-const Item = () => {
+import './Item.css'
+
+const Item = (props) => {
     return (
         <div className='item'>
+            <img src={props.image} alt="" />
+            <p>{props.name}</p>
+            <div className="item-price">
+                {props.price}
+            </div>
 
         </div>
     )
