bd5250287d48b76857f542ea26cc55f182c321d8
sauk1346 <sauk1346@gmail.com>
2024-10-18 22:42:14 -0300

    frontend: page LoginSignUp

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,6 +1,18 @@
+import './CSS/LoginSignup.css'
+
 const LoginSignup = () => {
     return (
-        <div>
+        <div className='loginsignup'>
+            <div className="loginsignup-container">
+                <h1>Sign up</h1>
+                <div className="loginsignup-fields">
+                    <input type="text" placeholder="Your Name"/>
+                    <input type="email" placeholder="Email Address"/>
+                    <input type="password" placeholder="Password"/>
+                </div>
+                <button>Continue</button>
+                <p className="loginsignup-login">Already have an account? <span>Login here</span></p>
+            </div> 
 
         </div>
     )
