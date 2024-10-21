feca17d9c4add7c742da0995694a04f20362facc
sauk1346 <sauk1346@gmail.com>
2024-10-20 21:14:20 -0300

    backend: endpoints

--- backend/index.js
+++ backend/index.js
@@ -104,6 +104,12 @@ app.post('/removeproduct', async (req,res) => {
     })
 })
 
+// Creating API for getting all products
+app.get('/allproducts',async (req,res) => {
+    let products = await Product.find({});
+    console.log("All Products Fetched");
+    res.send(products);
+})
 
 app.listen(port, (error) => {
     if (!error) {
